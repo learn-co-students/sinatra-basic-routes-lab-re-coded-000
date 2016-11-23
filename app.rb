46b2afd884b21c47f4536d37583d5634e2080ded
ALI ALRAWI <ali.developer90@gmail.com>
2016-11-23 07:20:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+       "Hello, World!"
+    end
+
+    get '/name' do
+       "My name is Ali !"
+    end
+    get '/hometown' do
+
+       "My hometown is Ramadi"
+    end
+    get '/favorite-song' do
+
+       "My favorite song is Waiting for you"
+    end
 end
